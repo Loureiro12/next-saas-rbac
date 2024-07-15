8a24181b585ebac3e2382dce7ca0305a289fb2a8
André Loureiro <contato.loureiro1@gmail.com>
2024-07-15 20:34:48 -0300

    Rota: Remover projeto

---
+++ apps/api/src/http/routes/projects/delete-project.ts
@@ -0,0 +1,67 @@
+import { projectSchema } from '@saas/auth'
+import { FastifyInstance } from 'fastify'
+import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+
+import { auth } from '@/http/middlewares/auth'
+import { prisma } from '@/lib/prisma'
+import { getUserPermissions } from '@/utils/get-user-permissions'
+
+import { BadRequestError } from '../_errors/bad-request-error'
+
+export async function deleteProject(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .delete(
+      '/organizations/:slug/projects/:projectId',
+      {
+        schema: {
+          tags: ['Projects'],
+          summary: 'Delete a project',
+          security: [{ bearerAuth: [] }],
+          params: z.object({
+            slug: z.string(),
+            projectId: z.string().uuid(),
+          }),
+          response: {
+            204: z.null(),
+          },
+        },
+      },
+      async (req, reply) => {
+        const { slug, projectId } = req.params
+
+        const { organization, membership } = await req.getUserMembership(slug)
+        const userId = await req.getCurrentUserId()
+
+        const project = await prisma.project.findUnique({
+          where: {
+            id: projectId,
+            organizationId: organization.id,
+          },
+        })
+
+        if (!project) {
+          throw new BadRequestError('Project not found')
+        }
+
+        const { cannot } = getUserPermissions(userId, membership.role)
+        const authProject = projectSchema.parse(project)
+
+        if (cannot('delete', authProject)) {
+          throw new BadRequestError(
+            'You do not have permission to delete this project',
+          )
+        }
+
+        await prisma.project.delete({
+          where: {
+            id: projectId,
+          },
+        })
+
+        return reply.status(204).send()
+      },
+    )
+}
